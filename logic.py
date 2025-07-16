895fc22a98ef049f2eac3e7018c510fd0b588ee6
Jorimam <iamgodwinjoewin@gmail.com>
2025-07-16 13:02:44 +0100

    Added more features to the calculator, multiplication, division and exponential

--- logic.py
+++ logic.py
@@ -20,6 +20,7 @@ add =float(first_number) + float(second_number)
 
 print(f"The sum of {first_number} and {second_number} is {add:,.2f}")
 print("----------------------------------")
+
 #Subtraction
 print("Enter two numbers to subtract")
 
@@ -33,3 +34,49 @@ sub =float(first_number) - float(second_number)
 
 print(f"The difference of {first_number} and {second_number} is {sub:,.2f}")
 print("----------------------------------")
+
+
+#Multiplication
+print("Enter two numbers to Multiply")
+
+print("First number")
+first_number = input("")
+
+print("Second number")
+second_number = input("")
+
+mul =float(first_number) * float(second_number)
+
+print(f"The the product of {first_number} and {second_number} is {mul:,.2f}")
+print("----------------------------------")
+
+
+#Division
+print("Enter two numbers to Divide")
+
+print("First number")
+first_number = input("")
+
+print("Second number")
+second_number = input("")
+
+div =float(first_number) / float(second_number)
+
+print(f" {first_number} divided by {second_number} is {div:,.2f}")
+print("----------------------------------")
+
+#Exponential
+print("Enter two numbers to carry out exponentiate")
+
+print("Base number")
+first_number = input("")
+
+print("Power")
+second_number = input("")
+
+exp =float(first_number) ** float(second_number)
+
+print(f" {first_number} divided by {second_number} is {exp:,.2f}")
+print("----------------------------------")
+
+
